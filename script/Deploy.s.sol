72874f9b474dd31cf18d0d0fa86f8b1a8bc6ad6b
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-12-11 23:55:14 +0100

    LPOracle: Simulate Order & _normalizePrices (#7)
    * LPOracle._simulatedOrder
    
    * normalizePrices & Test
    
    * add todo reference

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.25 <0.9.0;
 
-import { LPOracle } from "../src/LPOracle.sol";
-
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (LPOracle oracle) {
-        oracle = new LPOracle();
+    function run() public broadcast {
+        // TODO: https://github.com/lumoswiz/cow-amm-lp-oracle/issues/6
+        // oracle = new LPOracleFactory();
     }
 }
