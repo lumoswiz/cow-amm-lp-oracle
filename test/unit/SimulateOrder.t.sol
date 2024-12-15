1cb931d5894cb2dfbf1d69b2dddd0e7cbfea964c
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-12-15 22:22:43 +0100

    Testing simulatePoolReserves

--- test/unit/SimulateOrder.t.sol
+++ test/unit/SimulateOrder.t.sol
@@ -26,7 +26,7 @@ contract SimulateOrder_Unit_Test is BaseTest {
         _;
     }
 
-    function test_SimulateOrder() external whenNonZeroPrices {
+    function test_SimulateOrder_ImbalancedPool() external whenNonZeroPrices {
         // Set pool reserves (50:50 containing 15 and 3000 tokens)
         // This test is analogous to an imbalanced ETH/USDC pool with ETH at 300 and USDC at 1.
         // The pool has too much ETH!
@@ -43,5 +43,33 @@ contract SimulateOrder_Unit_Test is BaseTest {
         assertEq(TOKEN0, address(order.sellToken));
         // Pool needs to buy token1.
         assertEq(TOKEN1, address(order.buyToken));
+    }
+
+    function test_SimulateOrder_Balanced50_50Pool() external whenNonZeroPrices {
+        uint256 token0PoolReserve = 10e18;
+        uint256 token1PoolReserve = 10e18;
+        // Set prices
+        uint256 price0 = 1e8;
+        uint256 price1 = 1e8;
+
+        // set mock order for 50:50 pool
+        setMockOrder(token0PoolReserve, token1PoolReserve, 0.5e18);
+        GPv2Order.Data memory order = oracle.exposed_simulateOrder(price0, price1);
+        // Pool does not need to rebalance.
+        assertEq(order.sellAmount, 0);
+        assertEq(order.buyAmount, 0);
+    }
+
+    function test_SimulateOrder_Balanced10_90Pool() external whenNonZeroPrices {
+        uint256 token0PoolReserve = 10e18;
+        uint256 token1PoolReserve = 90e18;
+
+        // set mock order for 10:90 pool
+        setMockOrder(token0PoolReserve, token1PoolReserve, 0.1e18);
+        // Same Prices: 1e8
+        GPv2Order.Data memory order = oracle.exposed_simulateOrder(1e8, 1e8);
+        // Pool does not need to rebalance.
+        assertEq(order.sellAmount, 0);
+        assertEq(order.buyAmount, 0);
     }
 }
