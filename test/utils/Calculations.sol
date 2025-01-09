e143e615dccaf25d74c9c289868f7cbb48e737c2
lumoswiz <lumoswiz@protonmail.com>
2025-01-09 13:35:37 +0100

    test: add calcToken0FromToken1 helper for price feed balance calculations

--- test/utils/Calculations.sol
+++ test/utils/Calculations.sol
@@ -66,4 +66,29 @@ contract Calculations {
             ) / 1e18;
         }
     }
+
+    /// @dev Helper function that calculates the pool token 0 balance given:
+    ///   - price feed answers
+    ///   - pool token 0 normalized weight
+    ///   - pool token 1 balance
+    function calcToken0FromToken1(
+        uint8 feed0Decimals,
+        uint8 feed1Decimals,
+        int256 answer0,
+        int256 answer1,
+        uint256 weight0,
+        uint256 token1Balance
+    )
+        internal
+        pure
+        returns (uint256)
+    {
+        if (feed0Decimals == feed1Decimals) {
+            return (uint256(answer1) * token1Balance * weight0) / (uint256(answer0) * (1e18 - weight0));
+        } else {
+            uint256 adjustedAnswer0 = uint256(answer0) * 10 ** (18 - feed0Decimals);
+            uint256 adjustedAnswer1 = uint256(answer1) * 10 ** (18 - feed1Decimals);
+            return (adjustedAnswer1 * token1Balance * weight0) / (adjustedAnswer0 * (1e18 - weight0));
+        }
+    }
 }
