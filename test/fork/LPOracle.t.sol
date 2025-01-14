40b2502f9718de5dc7fc828363e2a90d8cecf1a7
lumoswiz <lumoswiz@protonmail.com>
2025-01-14 11:20:17 +0100

    test: add decimals fork test

--- test/fork/LPOracle.t.sol
+++ test/fork/LPOracle.t.sol
@@ -6,6 +6,18 @@ import { ForkTest } from "test/fork/Fork.t.sol";
 contract LPOracle_Fork_Test is ForkTest {
     constructor(address _token0, address _token1) ForkTest(_token0, _token1) { }
 
+    function test_Decimals() external view {
+        uint8 feed0Decimals = FORK_FEED0.decimals();
+        uint8 feed1Decimals = FORK_FEED1.decimals();
+        uint8 oracleDecimals = lpOracle.decimals();
+
+        if (feed0Decimals == feed1Decimals) {
+            assertEq(oracleDecimals, feed0Decimals);
+        } else {
+            assertEq(oracleDecimals, 18);
+        }
+    }
+
     function test_Descriptor() external view {
         string memory expectedDescription = string.concat(FORK_POOL.name(), " LP Token / USD");
         assertEq(lpOracle.description(), expectedDescription, "description");
