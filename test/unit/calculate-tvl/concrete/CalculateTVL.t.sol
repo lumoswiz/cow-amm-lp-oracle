374b94b9b9429e51ef4408446fe260406fa18bc4
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-01-16 14:04:36 +0100

    Add Unit Tests for Calculate TVL

--- test/unit/calculate-tvl/concrete/CalculateTVL.t.sol
+++ test/unit/calculate-tvl/concrete/CalculateTVL.t.sol
@@ -126,6 +126,7 @@ contract CalculateTVL_Concrete_Unit_Test is BaseTest {
 
     function test_TVL_MaxKValue_VeryHighAnswersAndHighFeedDecimals()
         external
+        pure
         whenValidBalance0
         whenValidBalance1
         whenKDoesNotOverflow
@@ -148,6 +149,7 @@ contract CalculateTVL_Concrete_Unit_Test is BaseTest {
 
     function test_TVL_MaxKValue_LowAnswersAndLowFeedDecimals()
         external
+        pure
         whenValidBalance0
         whenValidBalance1
         whenKDoesNotOverflow
@@ -168,6 +170,37 @@ contract CalculateTVL_Concrete_Unit_Test is BaseTest {
         assertGt(maxK, 1e40); // relatively high maximum k value
     }
 
+    function test_calculateTVL_SameDecimalTokens()
+        external
+        whenValidBalance0
+        whenValidBalance1
+        whenKDoesNotOverflow
+        whenAnswersPositive
+    {
+        int256 wethUsdPrice = 3000;
+        (int256 price0, int256 price1) = (wethUsdPrice * 1e8, 1e8);
+        // Balanced Pool with
+        setTokenBalances(defaults.TOKEN0_BALANCE(), defaults.TOKEN1_BALANCE());
+
+        uint256 tvl = oracle.exposed_calculateTVL(price0, price1);
+        assertApproxEqAbs(tvl, (2 * uint256(wethUsdPrice)) * 1e8, 2);
+    }
+
+    function test_calculateTVL_DifferentDecimalTokens()
+        external
+        whenValidBalance0
+        whenValidBalance1
+        whenKDoesNotOverflow
+        whenAnswersPositive
+    {
+        int256 wethUsdPrice = 3000;
+        (int256 price0, int256 price1) = (wethUsdPrice * 1e8, 1e8);
+        reinitOracle(18, 6);
+        setTokenBalances(1e18, uint256(wethUsdPrice) * 1e6);
+
+        uint256 tvl = oracle.exposed_calculateTVL(price0, price1);
+        assertApproxEqAbs(tvl, (2 * uint256(wethUsdPrice)) * 1e8, 2);
+    }
     // @todo potential to do some analysis on the 'allowed' variable space
     // Given pool weights and underlying assets in an expected price range,
     //   - what are acceptable balances?
